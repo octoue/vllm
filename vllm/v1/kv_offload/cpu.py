ca269ec2037ccb787b3d5590e7c77eac3524b61a
duck <wangshu72@mail.ustc.edu.cn>
2026-03-25 16:14:08 +0800

    修改参数顺序防止vllm启动出错

--- vllm/v1/kv_offload/cpu.py
+++ vllm/v1/kv_offload/cpu.py
@@ -103,11 +103,11 @@ class CPUOffloadingSpec(OffloadingSpec):
                 else 0
             )
             self._handlers = CpuGpuOffloadingHandlers(
-                attn_backends=attn_backends,
                 gpu_block_size=self.gpu_block_size,
                 cpu_block_size=self.offloaded_block_size,
                 num_cpu_blocks=self.num_blocks,
                 gpu_caches=kv_caches,
+                attn_backends=attn_backends,
                 max_concurrent_h2d=max_concurrent_h2d,
             )
 
